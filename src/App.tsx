0a4cbfcbb0b001bb883a099b113cc8697d53d3e6
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-11-04 13:20:41 -0300

    application structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import ViewPosts from './pages/ViewPosts';
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <ViewPosts/>
+    </>
+  );
+}
+
+export default App;
